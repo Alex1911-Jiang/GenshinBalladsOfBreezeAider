35904b5527ac38e7485e35e4481a9621c4652f82
Alex1911 <50268952+Alex1911-Jiang@users.noreply.github.com>
2021-03-19 19:10:10 +0800

    提交代码

---
+++ GenshinBalladsOfBreezeAider/frmMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace GenshinBalladsOfBreezeAider
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblStatusTitle = new System.Windows.Forms.Label();
+            this.groupGenshinProcess = new System.Windows.Forms.GroupBox();
+            this.lblWindowSize = new System.Windows.Forms.Label();
+            this.lblWindowSizeTitle = new System.Windows.Forms.Label();
+            this.lblWindowLocation = new System.Windows.Forms.Label();
+            this.lblWindowLocationTitle = new System.Windows.Forms.Label();
+            this.lblWindowType = new System.Windows.Forms.Label();
+            this.lblWindowTypeTitle = new System.Windows.Forms.Label();
+            this.groupGenshinProcess.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(121, 174);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 30);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始自动演奏";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(120, 23);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(89, 12);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "未找到原神进程";
+            // 
+            // lblStatusTitle
+            // 
+            this.lblStatusTitle.AutoSize = true;
+            this.lblStatusTitle.Location = new System.Drawing.Point(49, 23);
+            this.lblStatusTitle.Name = "lblStatusTitle";
+            this.lblStatusTitle.Size = new System.Drawing.Size(65, 12);
+            this.lblStatusTitle.TabIndex = 2;
+            this.lblStatusTitle.Text = "程序状态：";
+            // 
+            // groupGenshinProcess
+            // 
+            this.groupGenshinProcess.Controls.Add(this.lblWindowSize);
+            this.groupGenshinProcess.Controls.Add(this.lblWindowSizeTitle);
+            this.groupGenshinProcess.Controls.Add(this.lblWindowLocation);
+            this.groupGenshinProcess.Controls.Add(this.lblWindowLocationTitle);
+            this.groupGenshinProcess.Controls.Add(this.lblWindowType);
+            this.groupGenshinProcess.Controls.Add(this.lblWindowTypeTitle);
+            this.groupGenshinProcess.Location = new System.Drawing.Point(22, 53);
+            this.groupGenshinProcess.Name = "groupGenshinProcess";
+            this.groupGenshinProcess.Size = new System.Drawing.Size(297, 101);
+            this.groupGenshinProcess.TabIndex = 4;
+            this.groupGenshinProcess.TabStop = false;
+            this.groupGenshinProcess.Text = "原神窗口信息";
+            // 
+            // lblWindowSize
+            // 
+            this.lblWindowSize.AutoSize = true;
+            this.lblWindowSize.Location = new System.Drawing.Point(112, 71);
+            this.lblWindowSize.Name = "lblWindowSize";
+            this.lblWindowSize.Size = new System.Drawing.Size(29, 12);
+            this.lblWindowSize.TabIndex = 0;
+            this.lblWindowSize.Text = "未知";
+            // 
+            // lblWindowSizeTitle
+            // 
+            this.lblWindowSizeTitle.AutoSize = true;
+            this.lblWindowSizeTitle.Location = new System.Drawing.Point(6, 71);
+            this.lblWindowSizeTitle.Name = "lblWindowSizeTitle";
+            this.lblWindowSizeTitle.Size = new System.Drawing.Size(53, 12);
+            this.lblWindowSizeTitle.TabIndex = 0;
+            this.lblWindowSizeTitle.Text = "分辨率：";
+            // 
+            // lblWindowLocation
+            // 
+            this.lblWindowLocation.AutoSize = true;
+            this.lblWindowLocation.Location = new System.Drawing.Point(112, 46);
+            this.lblWindowLocation.Name = "lblWindowLocation";
+            this.lblWindowLocation.Size = new System.Drawing.Size(29, 12);
+            this.lblWindowLocation.TabIndex = 0;
+            this.lblWindowLocation.Text = "未知";
+            // 
+            // lblWindowLocationTitle
+            // 
+            this.lblWindowLocationTitle.AutoSize = true;
+            this.lblWindowLocationTitle.Location = new System.Drawing.Point(6, 46);
+            this.lblWindowLocationTitle.Name = "lblWindowLocationTitle";
+            this.lblWindowLocationTitle.Size = new System.Drawing.Size(77, 12);
+            this.lblWindowLocationTitle.TabIndex = 0;
+            this.lblWindowLocationTitle.Text = "左上角坐标：";
+            // 
+            // lblWindowType
+            // 
+            this.lblWindowType.AutoSize = true;
+            this.lblWindowType.Location = new System.Drawing.Point(112, 21);
+            this.lblWindowType.Name = "lblWindowType";
+            this.lblWindowType.Size = new System.Drawing.Size(29, 12);
+            this.lblWindowType.TabIndex = 0;
+            this.lblWindowType.Text = "未知";
+            // 
+            // lblWindowTypeTitle
+            // 
+            this.lblWindowTypeTitle.AutoSize = true;
+            this.lblWindowTypeTitle.Location = new System.Drawing.Point(7, 21);
+            this.lblWindowTypeTitle.Name = "lblWindowTypeTitle";
+            this.lblWindowTypeTitle.Size = new System.Drawing.Size(65, 12);
+            this.lblWindowTypeTitle.TabIndex = 0;
+            this.lblWindowTypeTitle.Text = "窗口状态：";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 216);
+            this.Controls.Add(this.groupGenshinProcess);
+            this.Controls.Add(this.lblStatusTitle);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "原神风物之歌辅助程序";
+            this.groupGenshinProcess.ResumeLayout(false);
+            this.groupGenshinProcess.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblStatusTitle;
+        private System.Windows.Forms.GroupBox groupGenshinProcess;
+        private System.Windows.Forms.Label lblWindowSize;
+        private System.Windows.Forms.Label lblWindowSizeTitle;
+        private System.Windows.Forms.Label lblWindowLocation;
+        private System.Windows.Forms.Label lblWindowLocationTitle;
+        private System.Windows.Forms.Label lblWindowType;
+        private System.Windows.Forms.Label lblWindowTypeTitle;
+    }
+}
+
---
+++ GenshinBalladsOfBreezeAider/frmMain.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenshinBalladsOfBreezeAider
+{
+    public partial class frmMain : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "FindWindow", CharSet = CharSet.Auto)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", EntryPoint = "keybd_event")]
+        public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowRect(IntPtr hwnd, out Rectangle lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetClientRect(IntPtr hwnd, out Rectangle lpRect);
+
+        [DllImport("user32")]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        private IntPtr hwndGenshin = IntPtr.Zero;
+
+        private int genshinWindowX = 0;
+        private int genshinWindowY = 0;
+        private int genshinWindowWdith = 0;
+        private int genshinWindowHeight = 0;
+        private bool working = false;
+
+        public frmMain() => InitializeComponent();
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            FindGenshinProcess();
+        }
+
+        private void FindGenshinProcess()
+        {
+            lblStatus.Text = $"未找到原神进程";
+            lblStatus.ForeColor = Color.Red;
+            Task.Run(() =>
+            {
+                while (!working)
+                {
+                    while (hwndGenshin == IntPtr.Zero)
+                    {
+                        hwndGenshin = FindWindow("UnityWndClass", "原神");
+                        Task.Delay(100).Wait();
+                    }
+                    GetWindowRect(hwndGenshin, out Rectangle windowRect);
+                    GetClientRect(hwndGenshin, out Rectangle clientRect);
+
+                    Invoke(new Action(() =>
+                    {
+                        try
+                        {
+                            if (!working)
+                            {
+                                lblStatus.Text = $"已找到原神进程，未开始自动演奏";
+                                lblStatus.ForeColor = Color.Black;
+                            }
+                            if (windowRect.X < -16000)  //全屏
+                            {
+                                genshinWindowX = 0;
+                                genshinWindowY = 0;
+                                genshinWindowWdith = Screen.PrimaryScreen.Bounds.Width;
+                                genshinWindowHeight = Screen.PrimaryScreen.Bounds.Height;
+
+                                lblWindowType.Text = "全屏";
+                                lblWindowLocation.Text = $"(0,0)";
+                                lblWindowSize.Text = $"{Screen.PrimaryScreen.Bounds.Width}×{Screen.PrimaryScreen.Bounds.Height}";
+                            }
+                            else  //窗口化
+                            {
+                                Rectangle tempRect = new Rectangle(windowRect.X, windowRect.Y, windowRect.Width - windowRect.X, windowRect.Height - windowRect.Y);
+                                Rectangle rect = new Rectangle(tempRect.X + (tempRect.Width - clientRect.Width) - 3, tempRect.Y + (tempRect.Height - clientRect.Height) - 3, clientRect.Width, clientRect.Height);
+
+                                lblWindowType.Text = "窗口化";
+                                lblWindowLocation.Text = $"({rect.X},{rect.Y})";
+                                lblWindowSize.Text = $"{rect.Width}×{rect.Height}";
+
+                                genshinWindowX = rect.X;
+                                genshinWindowY = rect.Y;
+                                genshinWindowWdith = rect.Width;
+                                genshinWindowHeight = rect.Height;
+                            }
+                            btnStart.Enabled = true;
+                        }
+                        catch
+                        {
+                        }
+                    }));
+                }
+            });
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (working)
+            {
+                working = false;
+                btnStart.Text = "开始自动演奏";
+                lblStatus.Text = $"已找到原神进程，未开始自动演奏";
+                lblStatus.ForeColor = Color.Black;
+                FindGenshinProcess();
+                return;
+            }
+            else
+            {
+                working = true;
+                lblStatus.Text = $"已开始自动演奏";
+                lblStatus.ForeColor = Color.Green;
+                btnStart.Text = "停止自动演奏";
+            }
+            Task.Run(() =>
+            {
+                while (working)
+                {
+                    Size s = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                    Bitmap memoryImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+                    memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+                    StartAutoPressKey(memoryImage, 0.253125, 0.37685, Keys.W);
+                    StartAutoPressKey(memoryImage, 0.253125, 0.823148, Keys.S);
+                    StartAutoPressKey(memoryImage, 0.127083, 0.6, Keys.A);
+                    StartAutoPressKey(memoryImage, 0.378125, 0.6, Keys.D);
+                    StartAutoPressKey(memoryImage, 0.74739583, 0.37685, Keys.I);
+                    StartAutoPressKey(memoryImage, 0.74739583, 0.823148, Keys.K);
+                    StartAutoPressKey(memoryImage, 0.62135416, 0.6, Keys.J);
+                    StartAutoPressKey(memoryImage, 0.872917, 0.6, Keys.L);
+
+                    memoryGraphics.Dispose();
+                    memoryImage.Dispose();
+                }
+            });
+        }
+
+        bool wReady, sReady, aReady, dReady, iReady, kReady, jReady, lReady;
+
+        private void StartAutoPressKey(Bitmap bmp, double scaleX, double scaleY, Keys key)
+        {
+            bool getReady = key switch
+            {
+                Keys.W => wReady,
+                Keys.S => sReady,
+                Keys.A => aReady,
+                Keys.D => dReady,
+                Keys.I => iReady,
+                Keys.K => kReady,
+                Keys.J => jReady,
+                Keys.L => lReady,
+                _ => throw new Exception()
+            };
+
+            int x = (int)Math.Round(genshinWindowWdith * scaleX) + genshinWindowX;
+            int y = (int)Math.Round(genshinWindowHeight * scaleY) + genshinWindowY;
+            PressKey(ref getReady, bmp, x, y, (byte)key);
+
+            switch (key)
+            {
+                case Keys.W:
+                    wReady = getReady;
+                    break;
+                case Keys.S:
+                    sReady = getReady;
+                    break;
+                case Keys.A:
+                    aReady = getReady;
+                    break;
+                case Keys.D:
+                    dReady = getReady;
+                    break;
+                case Keys.I:
+                    iReady = getReady;
+                    break;
+                case Keys.K:
+                    kReady = getReady;
+                    break;
+                case Keys.J:
+                    jReady = getReady;
+                    break;
+                case Keys.L:
+                    lReady = getReady;
+                    break;
+            }
+        }
+
+        private void PressKey(ref bool getReady, Bitmap bmp, int x, int y, byte key)
+        {
+            Color color = bmp.GetPixel(x, y);
+            if (!getReady)
+            {
+                if (color.R < 100 && color.G < 100 && color.B < 100)
+                {
+                    getReady = true;
+                }
+            }
+            if (getReady)
+            {
+                if (color.R == 255 && color.G > 170 && color.G < 230 && color.B > 60 && color.B < 70)
+                {
+                    //getReady = false;  //连击不会显示黑色圈导致判定有问题, 现只用作判断是否开始音游
+                    keybd_event(key, 0, 0, 0);
+                    Task.Delay(50).ContinueWith(_ =>
+                    {
+                        keybd_event(key, 0, 2, 0);
+                    });
+                }
+            }
+        }
+    }
+}
